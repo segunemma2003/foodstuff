d4f42a154c75994bbd5939519bbd23855a240eef
Segz <segz@Segzs-MacBook-Pro.local>
2024-01-24 18:33:14 +0100

    new

--- app/Http/Controllers/Main/LogicController.php
+++ app/Http/Controllers/Main/LogicController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\FoodStuff;
+use App\Models\Foodstuff as FoodStuff;
 use Cart;
 
 
